f18605353e42b7c31cfa19a2dd455e7f635bb565
myoungsc <myoungsc.dev@gmail.com>
2021-10-19 23:44:58 +0900

    add 9654"

---
+++ Problem_9000_9999/9654/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  9654
+//
+//  Created by 명수창 on 2021/10/19.
+//  Copyright © 2021 myoung. All rights reserved.
+//
+
+import Foundation
+
+print("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE")
+print("N2 Bomber      Heavy Fighter  Limited    21        ")
+print("J-Type 327     Light Combat   Unlimited  1         ")
+print("NX Cruiser     Medium Fighter Limited    18        ")
+print("N1 Starfighter Medium Fighter Unlimited  25        ")
+print("Royal Cruiser  Light Combat   Limited    4         ")
