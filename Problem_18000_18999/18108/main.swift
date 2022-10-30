8ad1384068bc008f3f75511edd9ebf5b34f743bb
myoungsc <myoungsc.dev@gmail.com>
2022-10-30 22:28:51 +0900

    * 18108

---
+++ Problem_18000_18999/18108/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  18108
+//
+//  Created by myoung on 2022/10/30.
+//
+
+
+import Foundation
+
+if let input = readLine() {
+    if let n = Int(input) {
+        print(n-543)
+    }
+}
+
---
+++ Problem_18000_18999/Problem_18000_18999/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Problem_18000_18999
+//
+//  Created by 명수창 on 2022/10/30.
+//
+
+import Foundation
+
+print("Hello, World!")
+
